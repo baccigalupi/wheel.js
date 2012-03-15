be2ebb79ba3b9b0c9164283c39ef5a3a375a9b7a
Kane Baccigalupi <baccigalupi@gmail.com>
2012-03-15 11:56:04 -0700

    refactoring the tests

--- spec/javascripts/ClassSpec.js
+++ spec/javascripts/ClassSpec.js
@@ -4,8 +4,9 @@ describe("Class", function() {
   });
 });
 
-describe("Function, subclassing", function() {
-  var SubClass, subclass;
+describe("Function", function() {
+  var SubClass, subclass, Subby, subby;
+
   beforeEach(function() {
     SubClass = Class.subclass({
       foo: 'bar',
@@ -14,76 +15,95 @@ describe("Function, subclassing", function() {
       classy: true,
       bar: function() { this.canHasSuper = true; }
     });
+
+    Subby = SubClass.subclass({
+      bof: 'bof',
+      foo: function() {
+        if ( this._super ) {
+          return 'Let there be foo';
+        }
+      },
+      zardoz: function() {
+        this._super();
+        if(this.hasMoxy) {
+          return "Zardoz has moxy!";
+        }
+      },
+    }, {
+      baz: 'baz',
+      classy: function() {
+        if ( this._super ) {
+          return 'I am classy';
+        }
+      },
+      bar: function() {
+        this._super();
+        if ( this.canHasSuper ) {
+          return 'I can has Super';
+        }
+      }
+    });
+
     subclass = new SubClass();
+    subby = new Subby();
   });
 
-  describe('class properties', function () {
-    it("has a class method subclass", function () {
-      expect(typeof Function.subclass).toBe('function');
-    });
-
-    it("has a reference to its super class", function () {
-      expect(SubClass.superclass).toBe(Class);
-    });
-
-    it("maintains existing class properties/methods", function () {
-      expect(SubClass.subclass).toBe(Class.subclass);
-    });
-
-    it("adds new properties/methods", function () {
-      expect(SubClass.classy).toBe(true);
-    });
-
-    it("can reference superclass methods", function () {
-      var Subby = SubClass.subclass({
-        zardoz: function() {
-          this._super();
-          if( this.hasMoxy ) {
-            return "Zardoz has moxy!"
-          }
-        }
+  describe("subclassing", function () {
+    describe('convenience and propigation attributes', function() {
+      it("has a class method subclass", function () {
+        expect(typeof Function.subclass).toBe('function');
       });
-      var subby = new Subby();
-      expect(subby.zardoz()).toBe("Zardoz has moxy!");
+
+      it("has a reference to its super class", function () {
+        expect(SubClass.superclass).toBe(Class);
+      });
+
+      it("passes on the subclass method during inheritance", function () {
+        expect(SubClass.subclass).toBe(Class.subclass);
+      });
     });
 
-    it("can reference superclass properties", function () {
-      var Subby = SubClass.subclass({
-        foo: function(str) {
-          return this._super + str;
-        }
+    describe("identity", function () {
+      it("is an instance of Function", function () {
+        expect(subclass instanceof Function).toBe(true);
       });
-      var subby = new Subby();
-      expect(subby.foo(' too')).toBe('bar too');
+
+      it("is an instance of its class", function () {
+        expect(subby instanceof SubClass).toBe(true);
+      });
+
+      it("is an instance of its superclass", function () {
+        expect(subby instanceof Subby).toBe(true);
+      });
+    });
+
+    describe("class attributes", function () {
+      it('gains new class attributes', function () {
+        expect(Subby.baz).toBe('baz');
+      });
+
+      it('maintains a reference to old class variables', function () {
+        expect(Subby.classy()).toBe('I am classy');
+      });
+
+      it('maintains a reference to old class methods', function () {
+        expect(Subby.bar()).toBe('I can has Super');
+      });
+    });
+
+    describe("instance attributes", function () {
+      it('gains new class attributes', function () {
+        expect(subby.bof).toBe('bof');
+      });
+
+      it('maintains a reference to old class variables', function () {
+        expect(subby.foo()).toBe('Let there be foo');
+      });
+
+      it('maintains a reference to old class methods', function () {
+        expect(subby.zardoz()).toBe('Zardoz has moxy!');
+      });
     });
   });
 
-  describe('instance properties', function () {
-    it("adds the instance properties that are passed in", function () {
-      expect(subclass.foo).toBe('bar');
-    });
-
-    it("maintains existing properties", function () {
-      var Sub2 = SubClass.subclass({baz: 'zardoz'});
-      var sub2 = new Sub2();
-      expect(sub2.foo).toBe('bar');
-    });
-
-    it('can reference superclass methods', function () {
-      var Subby = SubClass.subclass(undefined, {classy: function () { return !(this._super) }});
-      expect(Subby.classy()).toBe(false);
-    });
-
-    it('can reference superclass properties', function () {
-      var Subby = SubClass.subclass(undefined, {
-        bar: function () {
-          this._super();
-          if (this.canHasSuper) {
-            return 'I can has Super';
-          }
-        }
-      });
-      expect(Subby.bar()).toBe("I can has Super");
-    });
-  });
 });

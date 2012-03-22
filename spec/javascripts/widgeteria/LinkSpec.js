9e80fa77bbc2ce6a6299f3c9e64a4251242afbe1
Kane Baccigalupi <baccigalupi@gmail.com>
2012-03-22 16:05:22 -0700

    Link handle propogation and disability

--- spec/javascripts/widgeteria/LinkSpec.js
+++ spec/javascripts/widgeteria/LinkSpec.js
@@ -24,19 +24,40 @@ describe('jlisten.widgets.Link', function() {
       expect(linker.url).toBe('http://clunkerfoo.com');
     });
 
-    describe("this.propogate", function() {
+    describe("this.propagate", function() {
       it("is true by default", function () {
-        expect(linker.propogate).toBeTruthy();
+        expect(linker.propagate).toBeTruthy();
       });
 
       it("can be overwritten by initialization variables", function () {
-        linker = new Linker($(html).find('a'), {propogate: false});
-        expect(linker.propogate).toBe(false);
+        linker = new Linker($(html).find('a'), {propagate: false});
+        expect(linker.propagate).toBe(false);
+      });
+    });
+
+    describe("disabled", function () {
+      it("defaults to undefined", function (){
+        expect(linker.disabled).toBe(undefined);
+      });
+
+      it("will be true if it finds a disabled attribute", function () {
+        linker = new Linker($(html).find('a').attr('disabled', true));
+        expect(linker.disabled).toBe(true);
+      });
+
+      it("will consider the initialization options before the default or dom attribute", function () {
+        linker = new Linker($(html).find('a').attr('disabled', true), {disabled: false});
+        expect(linker.disabled).toBe(false);
       });
     });
   });
 
   describe("listen()", function() {
+    var event;
+    beforeEach(function() {
+      event = jQuery.Event("click");
+    });
+
     it("binds onClick to click events", function() {
       spyOn(linker, 'onClick');
       linker.$.trigger('click');
@@ -44,21 +65,49 @@ describe('jlisten.widgets.Link', function() {
     });
 
     it("prevents the default click behavior", function() {
-      var event = jQuery.Event("click");
       spyOn(event, 'preventDefault');
       linker.$.trigger(event);
       expect(event.preventDefault).toHaveBeenCalled();
     });
 
     describe("propegation", function() {
-      it("prevents propegation if this.propegate is false", function() {
-        
+       it("allows the propegation the click event by default", function() {
+        spyOn(event, 'stopPropagation');
+        linker.$.trigger(event);
+        expect(event.stopPropagation).not.toHaveBeenCalled();
       });
-      it("otherwise allows the propegation of the event");
+
+      it("prevents propegation if this.propegate is false", function() {
+        spyOn(event, 'stopPropagation');
+        linker.propagate = false;
+        linker.$.trigger(event);
+        expect(event.stopPropagation).toHaveBeenCalled();
+      });
     });
 
-    describe("when the dom has a disabled attribute", function (){
-      it("does not bind to onClick");
+    describe("when has an attribute disabled that is truthy", function (){
+      beforeEach(function() {
+        linker.disabled = true;
+      });
+
+      it("will not call onClick", function () {
+        spyOn(linker, 'onClick');
+        linker.$.trigger(event);
+        expect(linker.onClick).not.toHaveBeenCalled();
+      });
+
+      it("will cancel the default", function () {
+        spyOn(event, 'preventDefault');
+        linker.$.trigger(event);
+        expect(event.preventDefault).toHaveBeenCalled();
+      });
+
+      it("will stop propagation when specified", function () {
+        linker.propagate = false;
+        spyOn(event, 'stopPropagation');
+        linker.$.trigger(event);
+        expect(event.stopPropagation).toHaveBeenCalled();
+      });
     });
   });
 });

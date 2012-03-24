857622c101df0766a4c53055fd44eddc8b8d2787
Kane Baccigalupi <baccigalupi@gmail.com>
2012-03-23 19:11:49 -0700

    removing extra tests

--- spec/javascripts/widgeteria/AjaxLinkSpec.js
+++ spec/javascripts/widgeteria/AjaxLinkSpec.js
@@ -10,109 +10,24 @@ describe("jlisten.widgets.AjaxLink", function () {
     link = new Linker();
   });
 
-  describe("init", function (){
-    it("has the right class heirarchy", function() {
-      expect(link instanceof jlisten.widgets.Link).toBe(true);
-      expect(link instanceof jlisten.widgets.AjaxLink).toBe(true);
-    });
-
-    describe("httpMethod", function () {
-      it("defaults to 'get'", function () {
-        expect(link.httpMethod).toBe('get');
-      });
-
-      it("can be customized with initialization values", function () {
-        link = new Linker({httpMethod: 'put'});
-        expect(link.httpMethod).toBe('put');
-      });
-
-      it("can be set on the subclass", function () {
-        Linker.prototype.httpMethod = 'delete';
-        link = new Linker();
-
-        expect(link.httpMethod).toBe('delete');
-      });
-    });
-
-    describe("dataType", function () {
-      it("defaults to json", function () {
-        expect(link.dataType).toBe('json');
-      });
-
-      it("can be customized", function () {
-        link = new Linker({dataType: 'xml'});
-        expect(link.dataType).toBe('xml');
-      });
-    });
+  it("has the right class heirarchy", function() {
+    expect(link instanceof jlisten.widgets.Link).toBe(true);
+    expect(link instanceof jlisten.widgets.AjaxLink).toBe(true);
   });
+
+  it("mixes in Ajax", function () {
+    expect(link.send).toBe(jlisten.mixins.Ajax.send);
+  })
 
   describe("onClick", function () {
     var args, spy, data;
     beforeEach(function() {
-      link.httpMethod = 'delete';
-      link.dataType = 'xml';
-      data = {foo: 'bar'};
-      link.data = function() {return data};
-      spy = spyOn($, 'ajax');
+      spy = spyOn(link, 'send');
       link.onClick();
-      args = spy.argsForCall[0][0];
     });
 
-    it("calls $.ajax", function () {
-      expect($.ajax).toHaveBeenCalled();
-    });
-
-    it("uses the right url", function() {
-      expect(args.url).toBe('http://clinkity.com');
-    });
-
-    it("uses the httpMethod", function() {
-      expect(args.type).toBe('delete');
-    });
-
-    it("user the dataType", function () {
-      expect(args.dataType).toBe('xml');
-    });
-
-    it("sends data returned from the data() method", function () {
-      expect(args.data).toBe(data);
-    });
-
-    it("uses the object as context", function () {
-      expect(args.context).toBe(link);
-    });
-
-    it("registers a success handler", function () {
-      expect(args.success).toBe(link.onSuccess)
-    });
-
-    it("registers an error handler", function () {
-      expect(args.error).toBe(link.processError);
-    });
-
-    it("registers a complete handler", function () {
-      expect(args.complete).toBe(link.onCompletion);
-    });
-  });
-
-  describe('processError', function () {
-    beforeEach(function () {
-      spyOn(link, 'onError');
-    });
-
-    it("passes the parsed error data on to the onError method", function () {
-      var xhr = {responseText: JSON.stringify({status: 'foo'})};
-      link.processError(xhr);
-      expect(link.onError).toHaveBeenCalledWith({status: 'foo'});
-    });
-
-    it("passes on other json when that fails", function () {
-      var xhr = {
-        responseText: "]<span> not json at all {",
-        statusCode: function(){ return 500; }
-      };
-      link.processError(xhr);
-      expect(link.onError).toHaveBeenCalledWith({status: 500, message: "]<span> not json at all {"});
+    it("calls Ajax.send", function () {
+      expect(link.send).toHaveBeenCalled();
     });
   });
 });

fafa8adc578119df33ee8484fc0793f46fd5505d
Kane Baccigalupi <baccigalupi@gmail.com>
2012-03-25 13:23:30 -0700

    reorg to find a better standard for file names

--- spec/javascripts/helpers/SpecHelper.js
+++ spec/javascripts/helpers/SpecHelper.js
@@ -8,25 +8,3 @@ function fail() {
 function pend() {
   expect("pending test").toBe(false);
 }
-
-if (typeof sharedExamples == 'undefined' ) {
-  sharedExamples = {};
-}
-
-function itShouldBehaveLike() {
-  var exampleName      = _.first(arguments),
-      exampleArguments = _.select(_.rest(arguments), function(arg) { return !_.isFunction(arg); }),
-      innerBlock       = _.detect(arguments, function(arg) { return _.isFunction(arg); }),
-      exampleGroup     = sharedExamples[exampleName];
-
-  if(exampleGroup) {
-    return describe(exampleName, function() {
-      exampleGroup.apply(this, exampleArguments);
-      if(innerBlock) { innerBlock(); }
-    });
-  } else {
-    return it("cannot find shared behavior: '" + exampleName + "'", function() {
-      expect(false).toEqual(true);
-    });
-  }
-};

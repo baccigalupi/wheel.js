cc39937c2a5fcfd0ae14d2d374ba00e45fe88d60
Kane Baccigalupi <baccigalupi@gmail.com>
2012-03-25 13:37:37 -0700

    AjaxForm

---
+++ spec/javascripts/widgeteria/ajax_form_spec.js
@@ -0,0 +1,37 @@
+describe('Wheel.Widgeteria.AjaxForm', function() {
+  var Former, form;
+
+  beforeEach(function() {
+    Former = Wheel.Widgeteria.AjaxForm.subclass({},{
+      template: function() {
+        return "" +
+        "<form action='http://conformity.com'>" +
+        "  <input type='hidden' name='fit_in' value='true'/>" +
+        "  <input type='submit' value='Conform'/>" +
+        "</form>"
+      }
+    });
+    form = new Former();
+  });
+
+  it("has the right class heirarchy", function() {
+    expect(form instanceof Wheel.Widgeteria.Form).toBe(true);
+    expect(form instanceof Wheel.Widgeteria.AjaxForm).toBe(true);
+  });
+
+  it("mixes in Ajax", function () {
+    expect(form.send).toBe(Wheel.Mixins.Ajax.send);
+  })
+
+  describe("onSubmit", function () {
+    var args, spy, data;
+    beforeEach(function() {
+      spy = spyOn(form, 'send');
+      form.onSubmit();
+    });
+
+    it("calls Ajax.send", function () {
+      expect(form.send).toHaveBeenCalled();
+    });
+  });
+});

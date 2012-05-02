1473abf006b52a80150435a624598c28f37ca8e3
Kane Baccigalupi <baccigalupi@gmail.com>
2012-05-02 09:16:25 -0700

    redoing the tests for classes, doing some small refactoring

---
+++ spec/javascripts/class_helper_spec.js
@@ -0,0 +1,113 @@
+describe('Wheel.Class(), helper function', function() {
+  describe('with an id argument', function() {
+    beforeEach(function() {
+      Wheel.Class('Work', {
+        init: function() {
+          this.ergs = Math.floor(Math.random() * 100);
+        },
+
+        do: function(amount) {
+          if ( this.ergs > amount ) {
+            console.log('You don\'t have enough ergs!');
+          } else {
+            this.ergs -= amount
+          }
+        }
+      }, {
+        unit: 'erg'
+      });
+    });
+
+    afterEach(function() {
+      window.Work = undefined;
+    });
+
+    it('creates a class on window with that name', function() {
+      expect(typeof Work).toBe('function');
+    });
+
+    it('stores the id on the class', function() {
+      expect(Work.id).toBe('Work');
+    });
+
+    it('has class properties', function() {
+      expect(Work.unit).toBe('erg');
+    });
+
+    it('stores the proper prototype', function() {
+      expect(typeof Work.prototype.init).toBe('function');
+      expect(typeof Work.prototype.initialize).toBe('undefined');
+      expect(typeof Work.prototype.do).toBe('function');
+    });
+
+    it('indicates its inheritance structure', function() {
+      var work = new Work();
+      expect(work instanceof Work).toBe(true);
+      expect(work instanceof Wheel.ClassBuilder).toBe(true);
+    });
+
+    describe('with more complex id structurs', function() {
+      it('assigns it to the right place', function() {
+        window['Things'] = {};
+        Wheel.Class('Things.Better', {
+          betterThan: function(other) {
+            return true;
+          }
+        });
+        expect(Things.Better).not.toBe(undefined);
+        window['Things'] = undefined; // setting it back
+      });
+
+      it('will throw an error if the base does not exist', function() {
+        var error;
+        try {
+          Wheel.Class('Things.Better', {
+            betterThan: function(other) {
+              return true;
+            }
+          });
+        } catch(e) {
+          error = e;
+        }
+        expect(error).not.toBe(undefined);
+      });
+    });
+  });
+
+  describe('without an id argument', function() {
+    var OtherWork;
+    beforeEach(function() {
+      OtherWork = Wheel.Class({
+        init: function() {
+          this.ergs = Math.floor(Math.random() * 100);
+        },
+
+        do: function(amount) {
+          if ( this.ergs > amount ) {
+            console.log('You don\'t have enough ergs!');
+          } else {
+            this.ergs -= amount
+          }
+        }
+      }, {
+        unit: 'erg'
+      });
+    });
+
+    it('has class properties', function() {
+      expect(OtherWork.unit).toBe('erg');
+    });
+
+    it('stores the proper prototype', function() {
+      expect(typeof OtherWork.prototype.init).toBe('function');
+      expect(typeof OtherWork.prototype.initialize).toBe('undefined');
+      expect(typeof OtherWork.prototype.do).toBe('function');
+    });
+
+    it('indicates its inheritance structure', function() {
+      var work = new OtherWork();
+      expect(work instanceof OtherWork).toBe(true);
+      expect(work instanceof Wheel.ClassBuilder).toBe(true);
+    });
+  });
+});
---
+++ spec/javascripts/class_inheritance_spec.js
@@ -0,0 +1,78 @@
+describe('Wheel.Class, inheritance', function() {
+  var methods = {
+    base: function(value) {
+      return '`' + value + '`';
+    },
+
+    child: function(value) {
+      return "<div class='wrapper'>" + this._super(value) + "</div>";
+    },
+
+    grandchild: function(value) {
+      return "<div class='jacket'>" + this._super(value) + "</div>";
+    }
+  };
+
+  describe('instance methods chain through the ancestors', function() {
+    var repeater, wrapper, jacket;
+    beforeEach(function() {
+      Wheel.Class('Repeater', {
+        print: methods.base
+      });
+
+      Repeater.subclass('Wrapper', {
+        print: methods.child
+      });
+
+      Wrapper.subclass('Jacket', {
+        print: methods.grandchild
+      });
+
+      repeater = new Repeater();
+      wrapper = new Wrapper();
+      jacket = new Jacket();
+    });
+
+    it('base class has the right instance method', function() {
+      expect(repeater.print('foo')).toBe('`foo`');
+    });
+
+    it('children get the correct _super method in context', function() {
+      expect(wrapper.print('foo')).toBe("<div class='wrapper'>`foo`</div>");
+    });
+
+    it('grandchildren go all the way up the chain', function() {
+      expect(jacket.print('foo')).toBe("<div class='jacket'><div class='wrapper'>`foo`</div></div>");
+    });
+  });
+
+  describe('class methods chain through the ancestors', function() {
+    var repeater, wrapper, jacket;
+    beforeEach(function() {
+      Wheel.Class('Repeater', {},{
+        print: methods.base
+      });
+
+      Repeater.subclass('Wrapper', {},{
+        print: methods.child
+      });
+
+      Wrapper.subclass('Jacket', {}, {
+        print: methods.grandchild
+      });
+    });
+
+    it('base class has the right instance method', function() {
+      expect(Repeater.print('foo')).toBe('`foo`');
+    });
+
+    it('children get the correct _super method in context', function() {
+      expect(Wrapper.print('foo')).toBe("<div class='wrapper'>`foo`</div>");
+    });
+
+    it('grandchildren go all the way up the chain', function() {
+      expect(Jacket.print('foo')).toBe("<div class='jacket'><div class='wrapper'>`foo`</div></div>");
+    });
+  });
+
+});

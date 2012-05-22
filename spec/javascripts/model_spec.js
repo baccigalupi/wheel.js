ccbbd5d2b93de8e2af2a99f519c6b497f0fa6c1d
Kane Baccigalupi <baccigalupi@gmail.com>
2012-05-21 21:22:55 -0700

    attr accessors started for model properties

--- spec/javascripts/model_spec.js
+++ spec/javascripts/model_spec.js
@@ -40,22 +40,76 @@ describe('Wheel.Model', function() {
     });
   });
 
-  describe('attrAccessor(propName)', function() {
+  describe('_attrAccessor(propName)', function() {
+    var owner;
     beforeEach(function() {
       task = Task.create({
         name: 'Do some meta',
         state: 0,
         due_at: null
       });
+      owner = {name: "Kane"};
 
-      task.attrAccessor('owner');
+      task._attrAccessor('owner');
     });
 
     it('creates a prototype function with that name', function() {
       expect(typeof Task.prototype.owner == 'function').toBe(true)
     });
-  });
 
+    it('created function reads the underscore prefaced property', function() {
+      task._owner = owner;
+      expect(task.owner()).toBe(owner);
+    });
+
+    describe('when given an argument', function() {
+      it('it writes to the underscore prefaced property', function() {
+        task.owner(owner);
+        expect(task._owner).toBe(owner);
+      });
+
+      it('returns the value', function() {
+        expect(task.owner(owner)).toBe(owner);
+      });
+
+      describe('value has changed', function() {
+        beforeEach(function() {
+          spyOn(task, 'trigger');
+          task.owner(owner);
+        });
+
+        it('triggers a "change" event', function() {
+          expect(task.trigger).toHaveBeenCalled();
+        });
+
+        it('triggers an event related to the property changed', function() {
+          expect(task.trigger).toHaveBeenCalled();
+          expect(task.trigger.mostRecentCall.args[0]).toBe('change:owner');
+        });
+
+        it('marks the object as dirty', function() {
+          expect(task._dirty).toBe(true);
+        });
+      });
+
+      describe('value is the same', function() {
+        it('does not trigger any events', function() {
+          spyOn(task, 'trigger');
+          task.owner(task._owner);
+          expect(task.trigger).not.toHaveBeenCalled();
+        });
+      });
+    });
+
+    it('can handle multiple declarations in the same class', function() {
+      task._attrAccessor('tags');
+      var tags = ['neato', 'jazzy']
+      task.tags(tags);
+      task.owner(owner);
+      expect(task.tags()).toBe(tags);
+      expect(task.owner()).toBe(owner);
+    });
+  });
 
   xdescribe('save', function() {
     describe('object has not been saved', function() {

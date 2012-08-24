9f102191fce73e0986111385b9cc491cf6385a34
Kane Baccigalupi <baccigalupi@gmail.com>
2012-08-24 09:24:35 -0700

    working generators ???

--- lib/generators/wheel/install_generator.rb
+++ lib/generators/wheel/install_generator.rb
@@ -1,7 +1,10 @@
-class InstallDistGenerator < Rails::Generators::Base
-  source_root File.expand_path('../../../../wheel.js', __FILE__)
-  directory 'src', 'vendor/assets/javascript/wheel/src'
-  directory 'vendor', 'vendor/assets/javascript/wheel/vendor'
-  directory 'dist', 'vendor/assets/javascript/wheel/dist'
-  directory 'manifests', 'vendor/assets/javascript/wheel/manifests'
+class WheelGenerator < Rails::Generators::Base
+  source_root File.expand_path('../../../../', __FILE__) # the root of the project
+
+  def install
+    directory 'src', 'vendor/assets/javascript/wheel/src'
+    directory 'vendor', 'vendor/assets/javascript/wheel/vendor'
+    directory 'dist', 'vendor/assets/javascript/wheel/dist'
+    directory 'manifests', 'vendor/assets/javascript/wheel/manifests'
+  end
 end
